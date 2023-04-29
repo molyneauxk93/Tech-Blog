dd8dc45e94d010c54c7cd241f46fa9b8d40f4598
Josh Mol <joshua.molyneaux@live.com>
2023-04-28 22:48:28 -0400

    Adding updates to models utils and serverjs files

--- models/Blog.js
+++ models/Blog.js
@@ -5,7 +5,27 @@ class Blog extends Model { }
 
 Blog.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
     {
         sequelize,
--- models/Comment.js
+++ models/Comment.js
@@ -5,7 +5,28 @@ class Comment extends Model { }
 
 Comment.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
     },
     {
         sequelize,
