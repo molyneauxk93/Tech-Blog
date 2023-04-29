c49219b8c97d5aba93331af954bc697949b937c9
Josh Mol <joshua.molyneaux@live.com>
2023-04-28 23:07:52 -0400

    Adding index.js for models and update sql db schema

--- models/index.js
+++ models/index.js
@@ -2,6 +2,28 @@ const User = require("./User");
 const Blog = require("./Blog");
 const Comment = require('./Comment');
 
+//user can have many blog posts 
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//blog post belongs to a user
+Blog.belongsTo(User, {
+foreignKey: 'user_id'
+});
+
+//user can have many comments 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//comment belongs to a user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 
 
 module.exports = { User, Blog, Comment };
